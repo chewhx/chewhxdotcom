669f537443bf5c29d5ccce85282db521ee8c6445
Chew Han Xiang <5371541+chewhx@users.noreply.github.com>
2022-09-28 16:31:40 +0800

    feat: add 400 page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const NotFound = () => {
+  const router = useRouter();
+  React.useEffect(() => {
+    router.replace('/');
+  }, [router]);
+  return (
+    <>
+      <h1>404 Not Found</h1>
+    </>
+  );
+};
+
+export default NotFound;
