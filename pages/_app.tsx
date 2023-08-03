3066aef884a87f5412f6cfdabb1e361c7a3f87eb
Chew Han Xiang <5371541+chewhx@users.noreply.github.com>
2023-08-03 22:47:07 +0800

    :heavy_plus_sign: (lib, pages, styles, components.json, postcss, tailwind) install tailwindcss, shadcn-ui, cz-emoji

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import '../styles/index.scss';
+import '../styles/index.css';
 import AppContainer from '../layout/AppContainer';
 import ThemeProvider from '../providers/ThemeProvider';
 
