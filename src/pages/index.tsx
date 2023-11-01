03c1a63003bd353c231ed2baa604fc0716c75fb1
Chew Han Xiang <5371541+chewhx@users.noreply.github.com>
2023-11-01 16:42:21 +0800

    :sparkles: (src) new ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,103 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { LightbulbIcon, LightbulbOffIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { Inter } from 'next/font/google';
+import { Button } from '../components/ui/button';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+const portfolio: Array<{ title: string; link: string }> = [
+  {
+    title: 'GitHub',
+    link: 'https://github.com/chewhx',
+  },
+  {
+    title: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/chewhx/',
+  },
+  {
+    title: 'CV',
+    link: 'https://www.chewhx.com/cv.pdf',
+  },
+];
+
+const projects: Array<{
+  title: string;
+  link: string;
+  description: string;
+}> = [
+  {
+    title: 'Google Books API Wrapper',
+    link: 'https://www.npmjs.com/package/@chewhx/google-books',
+    description: 'JS Wrapper for Google Books API',
+  },
+  {
+    title: 'React Bootstrap Button',
+    link: 'https://www.npmjs.com/package/react-bootstrap-button',
+    description: 'Quickly implement a loading button for react bootstrap',
+  },
+  {
+    title: 'React useTodos',
+    link: 'https://github.com/chewhx/use-todo',
+    description: 'React package to create and manage todos state',
+  },
+];
 
 export default function Home() {
+  const { setTheme, theme } = useTheme();
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex flex-col items-center justify-between py-20 gap-y-6 mx-auto ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+      <div className='text-center'>
+        <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>
+          Chew Han Xiang
+        </h4>
+        <p className='text-sm text-muted-foreground'>Software Engineer</p>
+      </div>
+      <div className='flex'>
+        {portfolio.map(({ title, link }) => (
+          <a key={title} href={link} rel='noreferrer' target='_blank'>
+            <Button variant='link'>{title}</Button>
           </a>
-        </div>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='flex flex-col space-y-6 mx-auto w-auto md:w-[500px] px-12 py-6'>
+        {projects.map(({ title, description, link }) => (
+          <div className='flex flex-col gap-y-1' key={title}>
+            <a
+              href={link}
+              rel='noreferrer'
+              target='_blank'
+              className='text-sm font-medium leading-none underline cursor-pointer hover:text-slate-500'
+            >
+              {title}
+            </a>
+            <p className='text-sm text-muted-foreground'>{description}</p>
+          </div>
+        ))}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Button
+        size='icon'
+        variant='ghost'
+        onClick={() => {
+          if (theme === 'dark') {
+            setTheme('light');
+          } else {
+            setTheme('dark');
+          }
+        }}
+      >
+        {theme === 'dark' ? (
+          <LightbulbOffIcon size={18} />
+        ) : (
+          <LightbulbIcon size={18} />
+        )}
+      </Button>
+      <footer>
+        <small>&copy; 2023 Chew Han Xiang</small>
+      </footer>
     </main>
-  )
+  );
 }
